af6cafaafb67f7dcae251e05ece4de43d50efb4b
maksym <maksymus@gmail.com>
2018-01-27 16:19:55 -0500

    spring rest - extends ResponseEntityExceptionHandler to handle predefined exceptions

--- src/main/java/org/homenet/rest/quickpoll/controller/error/RestExceptionHandler.java
+++ src/main/java/org/homenet/rest/quickpoll/controller/error/RestExceptionHandler.java
@@ -1,11 +1,14 @@
 package org.homenet.rest.quickpoll.controller.error;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
@@ -13,8 +16,15 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+// extends to handle
+// @ExceptionHandler({NoSuchRequestHandlingMethodException.class, HttpRequestMethodNotSupportedException.class,
+// HttpMediaTypeNotSupportedException.class, HttpMediaTypeNotAcceptableException.class, MissingPathVariableException.class,
+// MissingServletRequestParameterException.class, ServletRequestBindingException.class, ConversionNotSupportedException.class,
+// TypeMismatchException.class, HttpMessageNotReadableException.class, HttpMessageNotWritableException.class,
+// MethodArgumentNotValidException.class, MissingServletRequestPartException.class, BindException.class,
+// NoHandlerFoundException.class, AsyncRequestTimeoutException.class})
 @ControllerAdvice
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException rnfe, HttpServletRequest request) {
@@ -27,22 +37,25 @@ public class RestExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorDetail);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, HttpServletRequest request) {
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
         ErrorDetail errorDetail = new ErrorDetail();
         errorDetail.setTimestamp(new Date().getTime());
-        errorDetail.setStatus(HttpStatus.BAD_REQUEST.value());
-        errorDetail.setTitle("Resource Not Found");
+        errorDetail.setStatus(status.value());
+        errorDetail.setTitle(status.getReasonPhrase());
         errorDetail.setDetails(ex.getMessage());
         errorDetail.setDeveloperMessage(ex.getClass().getName());
 
-        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
-        fieldErrors.forEach(err -> {
-            Map<String, List<ValidationError>> errors = errorDetail.getErrors();
-            errors.putIfAbsent(err.getField(), new ArrayList<>());
-            errors.get(err.getField()).add(new ValidationError(err.getCode(), err.getDefaultMessage()));
-        });
+        if (ex instanceof MethodArgumentNotValidException) {
+            MethodArgumentNotValidException manvEx = (MethodArgumentNotValidException) ex;
+            List<FieldError> fieldErrors = manvEx.getBindingResult().getFieldErrors();
+            fieldErrors.forEach(err -> {
+                Map<String, List<ValidationError>> errors = errorDetail.getErrors();
+                errors.putIfAbsent(err.getField(), new ArrayList<>());
+                errors.get(err.getField()).add(new ValidationError(err.getCode(), err.getDefaultMessage()));
+            });
+        }
 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorDetail);
+        return super.handleExceptionInternal(ex, errorDetail, headers, status, request);
     }
 }
